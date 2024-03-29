f759b54a28a5919fb7f8f209686c885c2b70314e
jiya1724 <jiyatrivedi204@gmail.com>
2024-03-30 01:26:41 +0530

    refact:Landing

--- frontend/src/screens/Landing.jsx
+++ frontend/src/screens/Landing.jsx
@@ -1,44 +1,23 @@
-import React from 'react'
-
-import logoBlue from "../assets/logo_Blue.png"
-import safe from '../assets/safe.png'
-import makeInIndia from '../assets/makeInIndia.png'
-import ssl from '../assets/ssl.png'
-
-
+import React,{useState, useEffect} from 'react'
+import PreLoader from '../components/PreLoader'
 
 const Landing = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
   return (
     <div>
-        <div className=" background w-[100vw] h-[100vh] flex justify-center items-end ">
-          <div className="assets flex h-[80%] flex-col justify-center items-center gap-[30%]">
-            <div className="logo flex justify-center items-center  ">
-              <img src={logoBlue} alt="logo" />
-            </div>
-            <div className='flex flex-col justify-center items-center gap-5 '>
-            <div className="other flex gap-[10px]">
-              <div>
-                <img src={safe} alt="" />
-              </div>
-              <div>
-                <img src={makeInIndia} alt="" />
-              </div>
-              <div>
-                <img src={ssl} alt="" />
-              </div>
+       {loading ? (
+        <PreLoader/>
+      ) : (
+        <div>Signup</div>
 
-            </div>
-            <div>
-              <p className=' font-bold text-xs text-darkGrey '><u>*Investments are subjected to market risks.</u></p>
-            </div>
-
-            </div>
-            
-          </div>
-          
-
-        </div>
-        
+      )}
+      
     </div>
   )
 }
