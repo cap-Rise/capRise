bdd4b4e2938aa38754b0eab08993ecfcb1aaceb3
jiya1724 <jiyatrivedi204@gmail.com>
2024-03-30 01:37:00 +0530

    refact:Landing

--- frontend/src/screens/Landing.jsx
+++ frontend/src/screens/Landing.jsx
@@ -1,25 +1,39 @@
-import React,{useState, useEffect} from 'react'
-import PreLoader from '../components/PreLoader'
+import React, { useState, useEffect } from 'react';
+import PreLoader from '../components/PreLoader';
+import SignUp from './SignUp';
 
 const Landing = () => {
   const [loading, setLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
     setLoading(true);
+
+
     setTimeout(() => {
       setLoading(false);
     }, 4000);
   }, []);
+
   return (
     <div>
-       {loading ? (
-        <PreLoader/>
+      {loading ? (
+        <PreLoader />
       ) : (
-        <div>Signup</div>
-
+        <div>
+          {isLoggedIn ? (
+            <div>
+              <p>Login</p>
+            </div>
+          ) : (
+            <div>
+              <SignUp/>
+            </div>
+          )}
+        </div>
       )}
-      
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
