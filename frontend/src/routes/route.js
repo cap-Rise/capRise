4941fbd8dfef224064da70beafcce5d109c87b0d
jiya1724 <jiyatrivedi204@gmail.com>
2024-03-30 22:24:28 +0530

    init:Auth,Pin

--- frontend/src/routes/route.js
+++ frontend/src/routes/route.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Landing from "../screens/Landing";
 import SignUp from "../screens/SignUp";
+import Auth from "../screens/Auth";
 const routes =[
     {
         path : "/",
         element: <Landing/>,
+    },
+    {
+        path : "/auth",
+        element: <Auth/>,
     },
     {
         path : "/signup",
--- frontend/src/screens/SignUp.jsx
+++ frontend/src/screens/SignUp.jsx
@@ -1,32 +1,12 @@
 import React from 'react'
 
-import svg from '../assets/signupSvg.svg'
-import logo from '../assets/logo_Blue.webp'
-import { Button } from '../components/Button'
-import gLogo from '../assets/googleLogo.png'
+
 
 
 const SignUp = () => {
   return (
-    <div className='background w-[100vw] h-[100vh] flex justify-center'>
-       <div className='flex flex-col gap-7 p-5 '>
-        <div className=" title flex flex-col gap-7  ">
-          <div className=" pt-12 flex justify-center items-center">
-              <img src={logo} className='w-[80%]' alt="" />
-          </div>
-          <div className='font-extrabold text-3xl text-darkGrey text-center'>SignUp</div>
-        </div>
-
-        <div>
-          <img src={svg} alt="" />
-        </div>
-        
-        <div className=' font-medium text-lightGrey text-center '>
-        By signing up you agree our <span className=' text-darkBlue cursor-pointer'><u>Terms and Conditions</u></span> 
-        </div>
-        
-       </div>
-
+    <div>
+      Signup form
     </div>
   )
 }
