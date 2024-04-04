372cbe07fb20b71e2bdbad12bf04c4ebcfcfe282
jiya1724 <jiyatrivedi204@gmail.com>
2024-04-05 01:23:53 +0530

    init:Navbar

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router'
 import authContext from '../context/authContext';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   const navigate = useNavigate();
   return (
     <div>
+      <Navbar/>
       <button onClick={()=>{
         navigate('/')
       }}>Clear History</button>
