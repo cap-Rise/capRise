f3ff234da12ff45a67f89f9efacd0b7d10e7027b
jiya1724 <jiyatrivedi204@gmail.com>
2024-04-05 22:22:26 +0530

    refact:NavBar

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -2,15 +2,34 @@ import React, { useContext } from 'react'
 import { useNavigate } from 'react-router'
 import authContext from '../context/authContext';
 import Navbar from '../components/Navbar';
+import rupee from '../assets/Rupee.png'
+import jiya from '../assets/jiya.png'
 
+import whiteLogo from '../assets/logo_white.png'
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div>
-      <Navbar/>
-      <button onClick={()=>{
-        navigate('/')
-      }}>Clear History</button>
+    <div className='h-[100vh] w-[100vw]'>
+      {/* NAVBAR */}
+      <div className=' absolute bottom-0 w-full'><Navbar /></div>
+
+      {/*HOME*/}
+      <div>
+        <div className="head bg-darkBlue p-5 flex justify-between w-full">
+          <div className="logo">
+            <img src={whiteLogo} alt="" />
+          </div>
+          <div className="icons flex gap-4">
+            <div className="addFunds">
+              <img src={rupee} alt="" />
+            </div>
+            
+              <img className='rounded-full w-[38px] h-[38px] border border-black' src={jiya} alt="" />
+            
+          </div>
+        </div>
+      </div>
+     
     </div>
   )
 }
