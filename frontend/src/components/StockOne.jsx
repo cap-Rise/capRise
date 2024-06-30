029454eb46ce5fe1d9af6cfd21425c5b813e56dc
Toyash <toyashpatil17@gmail.com>
2024-07-01 03:33:20 +0530

    feat:stock component

---
+++ frontend/src/components/StockOne.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import BuySellTag from './BuySellTag'
+import sbi from "../assets/SbiSvg.svg"
+
+const StockOne = ({stockName,quantity,ltp,amountUsed,type,imgSrc}) => {
+    return (
+        <div className=' shadow-lg drop-shadow-lg relative flex p-4 items-center justify-center h-[200px] w-[90%] rounded-[16px] bg-[#D9D9D9] '>
+            <div className='absolute top-0 right-8 '>
+                <BuySellTag type={type} />
+            </div>
+            <div className='h-full space-y-6 w-full'>
+                <div className=' flex items-center justify-start '>
+                    <div className=' gap-6 flex items-center justify-center'>
+                        <div className='flex items-center justify-center'>
+                            <img src={sbi}></img>
+                        </div>
+                        <div className='flex items-center justify-center'>
+                            <h1 className='font-bold text-[16px]' >{stockName}</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex items-center justify-between'>
+                    <div>
+                        <div className=' text-[#747474] text-[14px] flex items-center justify-center'>
+                            Quantity
+                        </div>
+                        <div className='flex items-center justify-center '>
+                            <h1 className='font-bold'>{quantity}</h1>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='h-[50px] w-[0.5px] bg-black'>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className=' text-[#747474] text-[14px] flex items-center justify-center'>
+                            Order Price
+                        </div>
+                        <div className='flex items-center justify-center '>
+                            <h1 className='font-bold'>{ltp}</h1>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='h-[50px] w-[0.5px] bg-black'>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className=' text-[#747474] text-[14px] flex items-center justify-center'>
+                            Total Amt.
+                        </div>
+                        <div className='flex items-center justify-center '>
+                            <h1 className='font-bold'>{amountUsed}</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex items-center justify-cente gap-4'>
+                    <div className='flex w-[50%] items-center justify-center'>
+                        <button className='bg-white border-solid border rounded-lg border-[#747474] text-[#060F66] w-[100%] p-2 ' onClick={() => { alert("Hello") }}>Cancel</button>
+                    </div>
+                    <div className='flex w-[50%] items-center justify-center'>
+                        <button className='bg-[#060F66] rounded-lg text-white w-[100%] p-2 ' onClick={() => { alert("Hello") }}>Modify</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StockOne
--- frontend/src/screens/Test.jsx
+++ frontend/src/screens/Test.jsx
@@ -3,11 +3,15 @@ import mastercard from "../assets/mastercard.svg"
 
 import CreditCard from '../components/CreditCard'
 import BuySellTag from '../components/BuySellTag'
+import sbi from "../assets/SbiSvg.svg"
+import StockOne from '../components/StockOne'
+
 const Test = () => {
     return (
         <div className='h-[100vh] w-[100vw] bg-blue-gray-500 overflow-hidden flex items-center justify-center '>
             {/* <CreditCard pnlBalance="50,000" name="Toyash Patil" /> */}
             {/* <BuySellTag type="BUY"/> */}
+            <StockOne stockName="State Bank Of India" type="Buy" quantity="10" ltp="1089" amountUsed="10890"  />
         </div>
     )
 }
