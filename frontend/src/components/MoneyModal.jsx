7a360c9bae8434cb6db4a9440ac7106af2c5195c
Toyash <toyashpatil17@gmail.com>
2024-07-06 03:11:06 +0530

    refactor: modal

--- frontend/src/components/MoneyModal.jsx
+++ frontend/src/components/MoneyModal.jsx
@@ -118,9 +118,9 @@ const MoneyModal = ({ isOpen, closeModal, Amount, orderId }) => {
 
     return (
         <div className="w-full h-full flex justify-center  items-end fixed top-0 left-0 bg-black bg-opacity-50 backdrop-filter backdrop-blur-sm z-50">
-            <div className=" rounded-tl-[40px] rounded-tr-[40px] border-[3px] border-solid border-lightGrey bg-white flex justify-center h-[25rem] z-1000 w-[100vw] relative">
-                <div>
-                    <button onClick={closeModal} ><div className=' z-50 absolute top-6 right-10'><img src={cross}></img></div></button>
+            <div className=" overflow-hidden modal-effect rounded-tl-[40px] rounded-tr-[40px] border-[3px] border-solid border-lightGrey bg-white  flex justify-center h-[25rem] z-1000 w-[100vw] relative">
+                <div className='absolute right-10 top-6  modal_up '>
+                    <button onClick={closeModal} ><div className='top-6 right-10'><img src={cross}></img></div></button>
                 </div>
                 <div className="modal-content absolute w-full  p-4 pr-6 pl-6 flex items-start mt-5 space-y-10 justify-center gap-6">
                     <div className=' w-full space-y-7'>
